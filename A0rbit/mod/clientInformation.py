ba2f6f4bf752e08b17dc104fe32a9305f998b3e3
DESKTOP-QQK9QQ8\Ignas <ignoxx@yahoo.de>
2018-09-01 13:35:55 +0100

    added basic information
    client information and packets IDS

---
+++ A0rbit/mod/clientInformation.py
@@ -0,0 +1,17 @@
+# This script contains the required client information,
+# in order to connect to the game 
+HOST = "www.andromeda-serv.com"
+
+# Right now you've to provide these information below,
+# in order to connect to the game
+# TODO: automate this part!
+UID = "41299" #UserID
+SID = "13224618e244ea0a51ae5cf4621829966807ee61" # SessionID
+CV  = "4.1" #ClientVersion
+
+# This is the map address, some servers might have different
+# IPs for different maps
+# This bot is specially made for andromeda 
+# andromeda has just one IP for all of their maps
+# TODO: automate this part too!
+ADDR = "54.37.129.54", 8080
